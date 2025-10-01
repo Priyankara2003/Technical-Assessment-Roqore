48b8a79be3b871cc9c9250b860a04066cc6ac077
srinath <srinathpriyankara77@gmail.com>
2025-10-01 08:36:18 +0530

    refactor: add comment.

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // decide port from environment variable
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
