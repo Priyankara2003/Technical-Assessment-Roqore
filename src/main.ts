bcc86b91a19aa4b7bc22d20c03d2666489695d55
srinath <srinathpriyankara77@gmail.com>
2025-10-01 09:13:58 +0530

    refactor: remove unwanted comments

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // decide port from environment variable
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
